a7dab86987e21a1dc02ff1e35312826d8c760ae4
Aristokay <412443267@qq.com>
2026-04-30 00:17:21 +0800

    first commit

---
+++ sky/sky-pojo/src/test/java/pojo/AppTest.java
@@ -0,0 +1,20 @@
+package pojo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
